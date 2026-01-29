923ea9d435cacbdac3f73551980a10934e17df7e
Daniel Alejandro Ortiz <dalejandroo@esliceu.net>
2026-01-29 15:48:11 +0100

    video 18

--- functions.php
+++ functions.php
@@ -1,17 +1,35 @@
 <?php
 
-function infobasic_config(){
-   
-register_nav_menus(
-     array(
-        'infobasic_main_menu' => 'Info Basic Menu Principal',     )
-);
+
+require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
+
+function infobasic_config() {
+
+    register_nav_menus( array(
+        'infobasic_main_menu' => 'Info Basic Menu Principal',
+    ) );
+
 }
 add_action( 'after_setup_theme', 'infobasic_config' );
 
+function infobasic_scripts() {
 
-function infobasic_scripts(){
-     wp_enqueue_script( 'bootstrap_js', get_theme_file_uri('inc/js/bootstrap.min.js'), array('jquery'), '4.5', true );
-     wp_enqueue_style( 'bootstrap_css', get_theme_file_uri('inc/css/bootstrap.min.css'), array(), '4.5', 'all' );
+    // Bootstrap JS (necesita jQuery)
+    wp_enqueue_script(
+        'bootstrap_js',
+        get_theme_file_uri('/inc/js/bootstrap.min.js'),
+        array('jquery'),
+        '4.5',
+        true
+    );
+
+    // Bootstrap CSS
+    wp_enqueue_style(
+        'bootstrap_css',
+        get_theme_file_uri('/inc/css/bootstrap.min.css'),
+        array(),
+        '4.5',
+        'all'
+    );
 }
 add_action( 'wp_enqueue_scripts', 'infobasic_scripts' );
--- header.php
+++ header.php
@@ -11,20 +11,20 @@
 
 <div class="cabecer">
     <nav class="navbar navbar-expand-md navbar-light bg-light">
-  <a class="navbar-brand" href="<?php echo site_url( "/") ?>">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
 
-  <?php
-        wp_nav_menu(
-          array(
-            'theme_location' => ''
-          )
-        )
-  ?>
-  
-  </div>
+    <?php    
+        wp_nav_menu( array(
+          'theme_location' => 'infobasic_main_menu',
+          'depth'          => 3, 
+          'container'      => false,
+          'menu_class'     => 'navbar-nav ml-auto',
+          'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
+          'walker'         => new WP_Bootstrap_Navwalker(),
+        ) );
+    ?>
+
 </nav>
 </div>
